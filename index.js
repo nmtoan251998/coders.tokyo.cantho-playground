aeee4110626f21b818763b6bc1d499063e6de389
nmtoan251998 <nmtoan251998@gmail.com>
2019-06-22 09:08:33 +0700

    feat: display joined and left users

--- index.js
+++ index.js
@@ -11,47 +11,26 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/user.html');
 });
 
-// Room
-let numUsers = 0;
-
 io.on('connect', function(socket) {   
-    let addedUser = false;
-
     socket.on('add user', function(username) {
-        if(addedUser) return;
-
         // store the username in the socket session for this client
-        socket.username = username;
-        ++numUsers;
-        addedUser = true;
+        socket.username = username;        
 
         // emit user joined event to specify that a user is connected
         socket.username = username;
-        users.unshift(socket.username);
+        users.unshift(socket.username);        
         updateUser('user joined', users);
     })        
 
     socket.on('disconnect', () => {        
         users.splice(
             parseInt(users.indexOf(socket.username)), 
-            1);        
+            1);
         updateUser('user left', users);
     });
 
-    // socket.on('disconnect', function() {
-    //     if(addedUser) --numUsers;
-
-    //     socket.emit('user left', {
-    //         username: socket.username,
-    //         numUsers: numUsers
-    //     })
-    //     users.splice(
-    //         parseInt(users.indexOf(socket.username)), 
-    //         1);
-    // });
-
     function updateUser(eventname, users) {
-        socket.emit(eventname, users);
+        io.emit(eventname, users);
     }
 });
 
--- public/script/socket-user.js
+++ public/script/socket-user.js
@@ -11,23 +11,24 @@ $(function() {
         console.log(message);
     }
 
-    function logCurrentConnection(users) {
-        let usersList = '';
+    function logCurrentConnection(users) {                
+        let usersList = ''
 
-        users.forEach(username => {
-            usersList += `<div class="user">${username}</div>`;
-        })
+        $('#users-container').html('');
+        
+        users.forEach(username => usersList += `<div class="user">${username}</div>`);
         
         $('#users-container').append(usersList);        
     }    
 
     socket.on('user joined', function(data) {        
-        logParticipant(data);        
+        logParticipant(data);
         logCurrentConnection(data);
     })
 
     socket.on('user left', function(data) {        
-        logParticipant(data);        
+        logParticipant(data);
+        logCurrentConnection(data);
     })
 
     $('#name-input').submit(function(e) {
@@ -37,10 +38,12 @@ $(function() {
         socket.emit('add user', $('#username').val().trim());
 
         // show playground button
-        // $('#playground-container').show();
+        $('#playground-container').show();        
         // show current online user
         $('#users-online').show();
-
+        // hide input field
+        $('#name-input-container').hide();
+        // clear the input value
         $('#username').val('');
         return false;
     });
